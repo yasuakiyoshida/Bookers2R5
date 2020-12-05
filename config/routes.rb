2422ae5c8acc360d457624489f1670c4002d5fbf
Yasuaki <yasu5293koh@gmail.com>
2020-12-05 01:07:27 +0000

    検索機能：ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,8 @@ Rails.application.routes.draw do
   	resource :relationships, only: [:create, :destroy]
   	get 'followings' => 'relationships#followings', as: 'followings'
   	get 'followers' => 'relationships#followers', as: 'followers'
+  	collection do
+      get :search
+    end
   end
 end
