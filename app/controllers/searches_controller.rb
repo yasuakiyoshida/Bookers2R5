20aad8d51fa30e0e6cc30a408224e2ce7fa5383d
Yasuaki <yasu5293koh@gmail.com>
2020-12-05 05:36:47 +0000

    検索機能：検索フォームにプルダウン追加、検索結果一覧ページ作成

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,8 @@
 class SearchesController < ApplicationController
   def search
+    @books = Book.where('title Like ? ', "#{params[:search]}") if params[:search].present?
   end
+  
+  # 完全一致検索 => モデル名.where('カラム名 like ?','検索したい文字列')
+  # 
 end
