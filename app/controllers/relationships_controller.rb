de53f4ee87bc37d288cc39328cbebaf5ec603984
Yasuaki <yasu5293koh@gmail.com>
2020-12-05 00:55:07 +0000

    検索(Search)機能：SearchesContoroller作成

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -10,12 +10,15 @@ class RelationshipsController < ApplicationController
 		redirect_to request.referer
 	end
   def followings
+  	# 自分を取得
 		user = User.find(params[:user_id])
+		# 自分がフォローしたユーザーを全て取得
 		@users = user.followings
   end
 
   def followers
 		user = User.find(params[:user_id])
+		# 自分のフォロワーを全て取得
 		@users = user.followers
   end
 end
